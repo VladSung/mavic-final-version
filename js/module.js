012854932dd2ca41f3e74443becce9605abff2ee
Vlad <67963328+VladSung@users.noreply.github.com>
2022-04-03 09:54:30 +0400

    Add files via upload

--- js/module.js
+++ js/module.js
@@ -1,53 +1,53 @@
 M.mod_quiz = M.mod_quiz || {};
-M.mod_quiz.init_attempt_form = function(Y) {
+M.mod_quiz.init_attempt_form = function (Y) {
     M.core_question_engine.init_form(Y, '#responseform');
     Y.on('submit', M.mod_quiz.timer.stop, '#responseform');
     M.core_formchangechecker.init({
         formid: 'responseform'
     })
 }
-;
-M.mod_quiz.init_review_form = function(Y) {
+    ;
+M.mod_quiz.init_review_form = function (Y) {
     M.core_question_engine.init_form(Y, '.questionflagsaveform');
-    Y.on('submit', function(e) {
+    Y.on('submit', function (e) {
         e.halt()
     }, '.questionflagsaveform')
 }
-;
-M.mod_quiz.init_comment_popup = function(Y) {
+    ;
+M.mod_quiz.init_comment_popup = function (Y) {
     var closebutton = Y.Node.create('<input type="button" />');
     closebutton.set('value', M.util.get_string('cancel', 'moodle'));
     Y.one('#id_submitbutton').ancestor().append(closebutton);
-    Y.on('click', function() {
+    Y.on('click', function () {
         window.close()
     }, closebutton)
 }
-;
+    ;
 M.mod_quiz.timer = {
     Y: null,
     endtime: 0,
     preview: 0,
     timeoutid: null,
-    init: function(Y, start, preview) {
+    init: function (Y, start, preview) {
         M.mod_quiz.timer.Y = Y;
         M.mod_quiz.timer.endtime = M.pageloadstarttime.getTime() + start * 1e3;
         M.mod_quiz.timer.preview = preview;
         M.mod_quiz.timer.update();
         Y.one('#quiz-timer').setStyle('display', 'block')
     },
-    stop: function(e) {
+    stop: function (e) {
         if (M.mod_quiz.timer.timeoutid)
             clearTimeout(M.mod_quiz.timer.timeoutid)
     },
-    two_digit: function(num) {
+    two_digit: function (num) {
         if (num < 10) {
             return '0' + num
         } else
             return num
     },
-    update: function() {
-        var Y = M.mod_quiz.timer.Y
-          , secondsleft = Math.floor((M.mod_quiz.timer.endtime - new Date().getTime()) / 1e3);
+    update: function () {
+        var Y = M.mod_quiz.timer.Y;
+        var secondsleft = Math.floor((M.mod_quiz.timer.endtime - new Date().getTime()) / 1e3);
         var hours = Math.floor(secondsleft / 3600);
         secondsleft -= hours * 3600;
         var minutes = Math.floor(secondsleft / 60);
@@ -57,10 +57,11 @@ M.mod_quiz.timer = {
         M.mod_quiz.timer.timeoutid = setTimeout(M.mod_quiz.timer.update, 100)
     }
 };
+
 M.mod_quiz.nav = M.mod_quiz.nav || {};
-M.mod_quiz.nav.update_flag_state = function(attemptid, questionid, newstate) {
+M.mod_quiz.nav.update_flag_state = function (attemptid, questionid, newstate) {
     var Y = M.mod_quiz.nav.Y
-      , navlink = Y.one('#quiznavbutton' + questionid);
+        , navlink = Y.one('#quiznavbutton' + questionid);
     navlink.removeClass('flagged');
     if (newstate == 1) {
         navlink.addClass('flagged');
@@ -68,8 +69,8 @@ M.mod_quiz.nav.update_flag_state = function(attemptid, questionid, newstate) {
     } else
         navlink.one('.accesshide .flagstate').setContent('')
 }
-;
-M.mod_quiz.nav.init = function(Y) {
+    ;
+M.mod_quiz.nav.init = function (Y) {
     M.mod_quiz.nav.Y = Y;
     Y.all('#quiznojswarning').remove();
     var form = Y.one('#responseform');
@@ -80,7 +81,7 @@ M.mod_quiz.nav.init = function(Y) {
             submit.set('name', '');
             submit.getDOMNode().click()
         }
-        ;Y.delegate('click', function(e) {
+        ; Y.delegate('click', function (e) {
             if (this.hasClass('thispage'))
                 return;
             e.preventDefault();
@@ -95,17 +96,17 @@ M.mod_quiz.nav.init = function(Y) {
             nav_to_page(pageno)
         }, document.body, '.qnbutton')
     }
-    ;if (Y.one('a.endtestlink'))
-        Y.on('click', function(e) {
+    ; if (Y.one('a.endtestlink'))
+        Y.on('click', function (e) {
             e.preventDefault();
             nav_to_page(-1)
         }, 'a.endtestlink');
     if (M.core_question_flags)
         M.core_question_flags.add_listener(M.mod_quiz.nav.update_flag_state)
 }
-;
+    ;
 M.mod_quiz.secure_window = {
-    init: function(Y) {
+    init: function (Y) {
         if (window.location.href.substring(0, 4) == 'file')
             window.location = 'about:blank';
         Y.delegate('contextmenu', M.mod_quiz.secure_window.prevent, document, '*');
@@ -116,10 +117,10 @@ M.mod_quiz.secure_window = {
         Y.delegate('cut', M.mod_quiz.secure_window.prevent, document, '*');
         Y.delegate('copy', M.mod_quiz.secure_window.prevent, document, '*');
         Y.delegate('paste', M.mod_quiz.secure_window.prevent, document, '*');
-        Y.on('beforeprint', function() {
+        Y.on('beforeprint', function () {
             Y.one(document.body).setStyle('display', 'none')
         }, window);
-        Y.on('afterprint', function() {
+        Y.on('afterprint', function () {
             Y.one(document.body).setStyle('display', 'block')
         }, window);
         Y.on('key', M.mod_quiz.secure_window.prevent, '*', 'press:67,86,88+ctrl');
@@ -129,7 +130,7 @@ M.mod_quiz.secure_window = {
         Y.on('key', M.mod_quiz.secure_window.prevent, '*', 'up:67,86,88+meta');
         Y.on('key', M.mod_quiz.secure_window.prevent, '*', 'down:67,86,88+meta')
     },
-    is_content_editable: function(n) {
+    is_content_editable: function (n) {
         if (n.test('[contenteditable=true]'))
             return true;
         n = n.get('parentNode');
@@ -137,27 +138,27 @@ M.mod_quiz.secure_window = {
             return false;
         return M.mod_quiz.secure_window.is_content_editable(n)
     },
-    prevent_selection: function(e) {
+    prevent_selection: function (e) {
         return false
     },
-    prevent: function(e) {
+    prevent: function (e) {
         alert(M.util.get_string('functiondisabledbysecuremode', 'quiz'));
         e.halt()
     },
-    prevent_mouse: function(e) {
+    prevent_mouse: function (e) {
         if (e.button == 1 && /^(INPUT|TEXTAREA|BUTTON|SELECT|LABEL|A)$/i.test(e.target.get('tagName')))
             return;
         if (e.button == 1 && M.mod_quiz.secure_window.is_content_editable(e.target))
             return;
         e.halt()
     },
-    init_close_button: function(Y, url) {
-        Y.on('click', function(e) {
+    init_close_button: function (Y, url) {
+        Y.on('click', function (e) {
             M.mod_quiz.secure_window.close(url, 0)
         }, '#secureclosebutton')
     },
-    close: function(Y, url, delay) {
-        setTimeout(function() {
+    close: function (Y, url, delay) {
+        setTimeout(function () {
             if (window.opener) {
                 window.opener.document.location.reload();
                 window.close()
